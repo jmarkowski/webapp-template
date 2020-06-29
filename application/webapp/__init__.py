d2ecd38d645a077e76ea471d1115c081e26fbc31
Jan Markowski <jan@markowski.ca>
2020-06-29 13:03:00 -0400

    Scrap the blueprints, don't enforce architecture because of Flask framework

--- application/webapp/__init__.py
+++ application/webapp/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from webapp.blueprints.example import page
+from webapp.example import page
 
 
 def create_app():
--- application/webapp/example/__init__.py
+++ application/webapp/example/__init__.py
@@ -1,2 +1,2 @@
 # All of your blueprint's __init__.py files will need this line
-from webapp.blueprints.example.routes import page
+from webapp.example.routes import page
