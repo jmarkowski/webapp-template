02838902cecdac6695179702d1baf0c9da177975
Jan Markowski <jan@markowski.ca>
2020-07-04 15:14:50 -0400

    Configure a custom folder for the instance directory (in pursuit of Flask agnosticism)

---
+++ application/secrets/settings.py
@@ -0,0 +1,2 @@
+# Do NOT add this file to version control
+SECRET_KEY = 'my-super-secret-key'
--- application/webapp/__init__.py
+++ application/webapp/__init__.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from webapp.main import main_bp
 from config import config_map
@@ -7,9 +9,19 @@ def create_app(app_config, override_settings=None):
     """
     Create a Flask application using the factory pattern.
     """
-    app = Flask(__name__)
+    secrets_path = os.path.abspath('./secrets')
+    app = Flask(__name__,
+                instance_path=secrets_path,
+                instance_relative_config=True)
 
     app.config.from_object(config_map[app_config])
+
+    # Load instance-specific overrides to the default configuration settings
+    # that have been loaded above from an object.
+    #
+    # With `silent=True`, Flask will not crash if the `settings.py` file
+    # does not exist.
+    app.config.from_pyfile('settings.py', silent=True)
 
     if override_settings:
         app.config.update(override_settings)
