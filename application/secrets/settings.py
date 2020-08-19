52296a0c192aa4cc13b7d4e7272666173c96125d
Jan Markowski <jan@markowski.ca>
2020-08-19 19:22:35 -0400

    Add example instructions for creating a SECRET_KEY

--- application/secrets/settings.py
+++ application/secrets/settings.py
@@ -1,2 +1,10 @@
 # Do NOT add this file to version control
+
+# A secret key is used for securely signing the session in a cookie and can be
+# used for other security related needs. It should be a long random string of
+# bytes.
+# For example, the output of the following command:
+# $ python -c 'import os; print(os.urandom(16))'
+#
+# It should go without saying: NEVER REVEAL THE SECRET KEY.
 SECRET_KEY = 'my-super-secret-key'
