91ef3e3b21af5fdbf57a32b20d00b3c3e064bd6b
Jan Markowski <jan@markowski.ca>
2020-08-20 08:59:54 -0400

    Rename 'invitation_model.py' to 'gateways.py' for generically holding the concrete data interface gateways

--- application/webapp/main/interactor.py
+++ application/webapp/main/interactor.py
@@ -5,7 +5,7 @@ from flask import render_template
 from flask import request
 
 from core.invitation import InvitationInteractor
-from webapp.invitation_model import InvitationGateway
+from webapp.gateways import InvitationGateway
 import util.time
 
 
