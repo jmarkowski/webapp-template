44021697cb54727942db6efdeb3038477e728c55
Jan Markowski <jan@markowski.ca>
2020-10-14 14:40:27 -0400

    Create base model mixin with created_on and updated_on fields to store TZ-aware UTC time updates

--- application/util/datetime.py
+++ application/util/datetime.py
@@ -1,5 +1,11 @@
 from datetime import datetime
+from datetime import timezone
 
 
 def now_str(format='%Y-%m-%d, %H:%M:%S'):
     return datetime.now().strftime(format)
+
+
+def now_tz_utc():
+    """Return an aware datetime object with the UTC as the timezone."""
+    return datetime.now(timezone.utc)
