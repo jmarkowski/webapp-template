899138fae73148ca9b5e86e10a6014c2e1bb1fac
Jan Markowski <jan@markowski.ca>
2022-06-01 18:37:17 -0400

    Add API contact example (missing form - need to add later)

--- application/webui/api/routes.py
+++ application/webui/api/routes.py
@@ -1,8 +1,14 @@
+import html
+
 from flask import Blueprint
 from flask import current_app
 from flask import jsonify
+from flask import request
 
 from webui.helpers import get_interactor
+from util.email import InvalidEmail
+from util.email import parse_email
+from util.email import send_email
 
 
 api_bp = Blueprint('api', __name__)
@@ -30,3 +36,71 @@ def invites():
     current_app.logger.info(f'Fetched invite list: {email_lst}')
 
     return jsonify(invite_dct)
+
+
+@api_bp.route('/api/contact', methods=['POST'])
+def contact():
+    data = request.get_json()
+
+    response = {
+        'success': False
+    }
+
+    name = data.get('name')
+    email = data.get('email')
+    subject = data.get('subject')
+    message = data.get('message')
+
+    if not name:
+        response['error'] = 'The name field is missing.'
+        return jsonify(response)
+
+    if not email:
+        response['error'] = 'The email is missing.'
+        return jsonify(response)
+
+    if not subject:
+        response['error'] = 'The subject is missing.'
+        return jsonify(response)
+
+    if not message:
+        response['error'] = 'There is nothing in the message body.'
+        return jsonify(response)
+
+    email_tokens = None
+
+    try:
+        email_tokens = parse_email(email)
+    except InvalidEmail:
+        response['error'] = f'Oops, {email} appears to be an invalid email.'
+        return jsonify(response)
+
+    if email_tokens:
+        message = html.escape(message) # Remove possible tags in the email
+        message = message.replace('\n', '<br>') # New lines
+
+        product = current_app.config['INFO']['product']
+
+        subject = f'[{product}] {subject}'
+        body = f"""
+<!DOCTYPE html>
+<html>
+<body>
+<em>From:</em> {name} (<a href="mailto:{email}">{email}</a>)
+<br>
+<em>Message:</em>
+<br><br>
+{message}
+</body>
+</html>
+"""
+        email_sent = send_email(subject, body, current_app.config)
+
+        if not email_sent:
+            response['error'] = 'Sorry, due to technical ' \
+                'difficulties we were unable to forward your message ' \
+                'at this time. '
+
+        response['success'] = True
+
+    return jsonify(response)
