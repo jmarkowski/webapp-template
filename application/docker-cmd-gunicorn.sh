73b61fc953bd93f00f7608e33a9d6c28a2ed30aa
Jan Markowski <jan@markowski.ca>
2022-06-23 12:41:48 -0400

    fixup! Use docker secrets to pass in sensitive database information

--- application/docker-cmd-gunicorn.sh
+++ application/docker-cmd-gunicorn.sh
@@ -10,6 +10,11 @@ ACCESS_LOG_FMT='%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
 
 mkdir -p ${LOG_DIR}
 
+# Set the database environment settings
+export POSTGRES_DB=${DB_NAME}
+export POSTGRES_USER=$(cat /run/secrets/db_user)
+export POSTGRES_PASSWORD=$(cat /run/secrets/db_password)
+
 exec gunicorn \
     --bind :${FLASK_PORT} \
     --workers=${WSGI_WORKERS} \
